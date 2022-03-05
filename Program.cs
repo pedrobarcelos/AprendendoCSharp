ad32fc06d63dd7a63704991e0f7a618dba75911b
Pedro Barcelos <pbarcelos56@icloud.com>
2022-03-04 23:44:30 -0300

    feat: Hello World

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AprendendoCSharp
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Aperte Enter para finalizar!");
+            Console.ReadLine();
+        }
+    }
+}
